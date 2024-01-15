17d69e81a7f50e5c768f18c392264e7067eab9b8
Shinde Rahul <shinde2732000@gmail.com>
2024-01-15 14:58:06 +0530

    partially functioning frontend done

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+TYPE = (
+    ("Positive",'Positive'),
+    ('Negative','Negative')
+    )
 
 # Create your models here.
+class Expense(models.Model):
+    name = models.CharField(max_length=100)
+    Expense_type = models.CharField(max_length=8, choices=TYPE)
+    amount = models.FloatField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    income = models.FloatField()
+    expenses = models.FloatField(blank=True,default=0)
+    balance = models.FloatField(blank=True,null=True)
--- home/views.py
+++ home/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from .models import Expense, Profile
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    if request.method == "POST":
+        text = request.POST.get('text')
+        amount = request.POST.get('amount')
+        expense_type = request.POST.get('expense_type')
+        print(text)
+        print(amount)
+        print(expense_type)
+    profile = Profile.objects.filter(user=request.user).first()
+    context = {"profile": profile}
+    print(request.user)
+    print(profile)
+    print(context)
+    return render(request, 'home/home.html',context)
