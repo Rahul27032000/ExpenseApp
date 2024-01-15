1819eaf222e0597d4f714db4826e218c0ef09391
Shinde Rahul <shinde2732000@gmail.com>
2024-01-15 16:24:55 +0530

    added logic for expense history and requirements.txt file

--- home/views.py
+++ home/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Expense, Profile
 
 # Create your views here.
 def home(request):
     profile = Profile.objects.get(user=request.user)
-
+    expenses = Expense.objects.filter(user=request.user)
     if request.method == "POST":
         text = request.POST.get('text')
         amount = int(request.POST.get('amount'))
         expense_type = request.POST.get('expense_type')
         
-        
+        expense = Expense(name=text, amount=amount,user=request.user, expense_type=expense_type)
+        expense.save()
         if expense_type == "Positive":
             profile.balance += amount
         else:
@@ -19,8 +20,9 @@ def home(request):
             
         
         profile.save()
+        return redirect("/")
         
-    context = {"profile": profile}
+    context = {"profile": profile,"expenses": expenses}
     print(request.user)
     print(profile)
     print(context)
