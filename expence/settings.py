ea3be1d3958ce728d517c2251f87df5d2fff35a5
Rahul <heyrahul327@gmail.com>
2024-02-13 16:12:35 +0530

    added some changes in style and added .env file

--- expence/settings.py
+++ expence/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +24,10 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$$%&87%v4f1lgs2e$qu!ay6y5srz20rjua=abgh-=a24p6c1!o'
+SECRET_KEY = Config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Config('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
